5f18d3f8efae10ff9d58116a1a8bc96106f2f252
Khaled Nobani <ikhalednobani@icloud.com>
2021-04-28 15:45:25 +0700

    Authentication Module (Signup) Screen
    A new screen of Signup is embedded in the same code of signin,
    the screen is the screen for new users into the system in which they
    enter personal information for a new account.

--- lib/main.dart
+++ lib/main.dart
@@ -93,6 +93,101 @@ class _MyHomePageState extends State<MyHomePage> {
                 print('Sigin');
               },
             ),
+            Container(
+              alignment: Alignment(-1.0, 0.0),
+              margin: EdgeInsets.only(top: 24.0),
+              child: Row(children: [
+                Text(
+                  "Don't have an account? ",
+                ),
+                TextButton(
+                  child: Text('Create a new one!'),
+                  onPressed: () {
+                    // Take to signup screen
+                    Navigator.of(context).push(MaterialPageRoute(builder: (context) => Signup()));
+                  },
+                )
+              ])
+            ),
+          ])
+        ),
+      ]),
+    );
+  }
+}
+
+class Signup extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Expense Manager')),
+      body: Column(children: [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+          child: Column(children: [
+            Container(
+              margin: EdgeInsets.only(bottom: 8.0),
+              child: Text(
+                'Signup',
+                style: TextStyle(
+                  fontSize: 24,
+                )
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(bottom: 16.0),
+              child: Text(
+                'Create a new account with Expense Manager',
+                style: TextStyle(color: Colors.black.withOpacity(0.54)),
+              ),
+            ),
+            TextFormField(
+              cursorColor: Theme.of(context).cursorColor,
+              decoration: InputDecoration(
+                icon: Icon(Icons.account_circle),
+                labelText: 'Fullname',
+                helperText: 'Your first and last name',
+              ),
+              initialValue: '',
+            ),
+            TextFormField(
+              cursorColor: Theme.of(context).cursorColor,
+              decoration: InputDecoration(
+                icon: Icon(Icons.account_circle),
+                labelText: 'Username',
+                helperText: 'Your email address @',
+              ),
+              initialValue: '',
+              validator: (val) => val.isEmpty || !val.contains('@') ? 'Please, enter valid email address' : null,
+            ),
+            Container(
+              margin: EdgeInsets.only(bottom: 24.0),
+              child: TextFormField(
+                cursorColor: Theme.of(context).cursorColor,
+                decoration: InputDecoration(
+                  icon: Icon(Icons.lock),
+                  labelText: 'Password',
+                  helperText: 'Your password',
+                ),
+                initialValue: '',
+                obscureText: true,
+              ),
+            ),
+            ElevatedButton(
+              child: Container(
+                child: Text(
+                  'Singup'.toUpperCase(),
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                width: MediaQuery.of(context).size.width,
+              ),
+              onPressed: () {
+                print('Creating new account');
+              },
+            ),
           ])
         ),
       ]),
